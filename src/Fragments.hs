85fe5c05b9e2c0d34089a0950d5288162c815368
Nick Chapman <nic@amadido.co.uk>
2023-04-01 08:11:46 +0100

    move OverMain in Fragments

--- src/Fragments.hs
+++ src/Fragments.hs
@@ -1,18 +1,47 @@
+{-# LANGUAGE RebindableSyntax #-}
+{-# OPTIONS -Wno-missing-signatures #-}
 
-module Fragments (top) where
+module Fragments (main) where
 
-import Prelude hiding (pure)
-import Asm
---import Data.Word (Word8)
+import Data.Word8 (Word8) -- so the name in printed inferred types is not qualified
+import Prelude hiding (return,pure,(>>=),(>>))
+import OverThink
 
-top :: ()
-top = ()
-  where
+_w :: Word8 -- so the import is not redundant
+_w = undefined
 
-    _frag1 = Asm.do
-      v <- allocateZP
-      lda (immChar 'c')
-      sta (ZeroPage v)
-      --lda (immediate 11) -- TEST: this should be a type error
-      sta (ZeroPage v)
+eff :: Asm g ('Gen z ('Seq ('Code cpu op cpu2) h)) v -> Effect cpu v
+eff = undefined
+data Effect (cpu :: CpuState) v
+
+zpType :: Byte ('ZpAddr ('Seq i is)) -> Byte i
+zpType = undefined
+
+
+main c1 = eff $ do
+  --c2 <- Label
+  lda_i (immChar 'x')
+  sta_a c1
+  pure c1
+
+
+_frag2 = eff $ do
+  v1 <- AllocZP
+  v2 <- AllocZP
+  lda_i (immChar 'x')
+  sta_z v1
+  lda_i (immWord 123)
+  sta_z (nextZ v1) -- show access to subsequent addresses
+  -- sta_z v1 -- type error NICE
+  pure (zpType v2)
+
+
+_frag1 = eff $ do -- from from original Asm
+  v <- AllocZP
+  lda_i (immChar 'c')
+  sta_z v
+  --lda_i (immWord 11) -- type error NICE
+  sta_z v
+
+
 
